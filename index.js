b886f8d9c7c8d02e06729f4866a18bd91dd53e7b
LaggySoul <trcalaquian@gmail.com>
2023-05-10 12:45:51 -0700

    Initialized files that need to be done this week

---
+++ index.js
@@ -0,0 +1,62 @@
+require("./utils.js");
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const bcrypt = require("bcrypt");
+
+const port = 3000;
+
+const Joi = require("joi");
+
+const expireTime = 60 * 60 * 1000;
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var { database } = include("databaseConnection");
+
+const userCollection = database.db(mongodb_database).collection("users");
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+var mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+  dbName: "sessions",
+});
+
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+app.listen(port, () => {
+  console.log("Node application listening on port " + port);
+});
