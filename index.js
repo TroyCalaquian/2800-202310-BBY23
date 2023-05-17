f8b5d8492b05a45ac5290643d685d575cccb6cb0
LaggySoul <trcalaquian@gmail.com>
2023-05-17 15:07:19 -0700

    Implemented search function

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 var { database } = include("databaseConnection");
 
 const userCollection = database.db(mongodb_database).collection("users");
-const playlistCollection = database.db(mongodb_database).collection("playlists");
+const playlistCollection = database
+  .db(mongodb_database)
+  .collection("playlists");
 
 app.set("view engine", "ejs");
 
@@ -60,165 +62,184 @@ function hasSession(req, res, next) {
   }
 }
 
+app.get("/", (req, res) => {
+  var sessionState = req.session.authenticated;
+  var username = req.session.name;
 
-app.get('/', (req,res) => {
-    var sessionState = req.session.authenticated;
-    var username = req.session.name;
-
-    res.render("welcome", {isLoggedIn: sessionState, userName: username});
+  res.render("welcome", { isLoggedIn: sessionState, userName: username });
 });
 
-app.get('/login', (req,res) => {
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/loggingin", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  const schema = Joi.string().email().required();
+  const validationResult = schema.validate(email);
+
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.redirect("/login");
+    return;
+  }
+
+  const result = await userCollection
+    .find({ email: email })
+    .project({ email: 1, password: 1, _id: 1 })
+    .toArray();
+
+  console.log(result);
+  if (result.length != 1) {
+    console.log("user not found");
+    res.redirect("/login");
+    return;
+  }
+  if (await bcrypt.compare(password, result[0].password)) {
+    console.log("correct password");
+    req.session.authenticated = true;
+    req.session.email = email;
+    req.session.cookie.maxAge = expireTime;
+    var user = await userCollection.findOne({ email: email });
+    req.session.name = user.username;
+    req.session.user = user;
+    // console.log(user)
+    // console.log("###########################")
+    // console.log(req.session.email)
+    res.redirect("/loggedIn");
+    return;
+  } else {
+    console.log("incorrect password");
+    res.render("loginfail");
+    return;
+  }
+});
+
+app.get("/loggedin", hasSession, (req, res) => {
+  res.redirect("/welcome");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// After signup, posts to this
+
+app.post("/submitUser", async (req, res) => {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var securityAnswer = req.body.answer;
+
+  const schema = Joi.object({
+    username: Joi.string().alphanum().max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().max(20).required(),
+    securityAnswer: Joi.string().alphanum().max(20).required(),
+  });
+
+  const validationResult = schema.validate({
+    username,
+    email,
+    password,
+    securityAnswer,
+  });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    var error = validationResult.error.details[0].message;
+    res.render("signupfail", { theError: error });
+    return;
+  }
+
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  await userCollection.insertOne({
+    username: username,
+    password: hashedPassword,
+    email: email,
+    Security_Question_Answer: securityAnswer,
+    user_type: "user",
+    pfp: null,
+    playlists: [],
+  });
+  console.log("Inserted user");
+
+  res.redirect("/welcome");
+});
+
+app.get("/welcome", hasSession, (req, res) => {
+  res.render("welcome", {
+    isLoggedIn: req.session.authenticated,
+    userName: req.session.name,
+  });
+});
+
+app.get("/changePassword", (req, res) => {
+  var sessionState = req.session.authenticated;
+  if (sessionState) {
+    res.render("resetPassword", { sessionState: sessionState });
+  } else {
+    res.render("securityQuestion");
+  }
+});
+
+app.post("/changingPassword", async (req, res) => {
+  var newpassword = req.body.password;
+  var useremail = req.session.email;
+  console.log(useremail);
+  let currentUser = await userCollection.findOne({ email: useremail });
+  console.log("hello");
+  console.log(currentUser);
+  console.log(currentUser.email);
+  console.log(currentUser.password);
+  var hashedPassword = await bcrypt.hash(newpassword, saltRounds);
+  await userCollection.updateOne(
+    { email: useremail },
+    { $set: { password: hashedPassword } }
+  );
+  console.log("=========NEW PASS=========");
+  console.log(newpassword);
+  console.log(hashedPassword);
+
+  if (req.session.authenticated) {
+    console.log(currentUser.username + "going to welcome");
+
+    res.render("welcome", {
+      userName: currentUser.username,
+      isLoggedIn: req.session.authenticated,
+    });
+  } else {
     res.render("login");
+  }
 });
 
-app.post('/loggingin', async (req,res) => {
-    var email = req.body.email;
-    var password = req.body.password;
-
-    const schema = Joi.string().email().required();
-    const validationResult = schema.validate(email);
-    
-    if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/login");
-      return;
-    }
-
-    const result = await userCollection.find({email: email}).project({email: 1, password: 1, _id: 1}).toArray();
-
-    console.log(result);
-    if (result.length != 1) {
-      console.log("user not found");
-      res.redirect("/login");
-      return;
-    }
-    if (await bcrypt.compare(password, result[0].password)) {
-      console.log("correct password");
-      req.session.authenticated = true;
-      req.session.email = email;
-      req.session.cookie.maxAge = expireTime;
-      var user = await userCollection.findOne({ email: email });
-        req.session.name = user.username;
-        req.session.user = user;
-      // console.log(user)
-      // console.log("###########################")
-      // console.log(req.session.email)
-      res.redirect('/loggedIn');
-      return;
-    }
-    else {
-        console.log("incorrect password");
-        res.render("loginfail");
-        return;
-    }
-});
-
-app.get('/loggedin', hasSession, (req,res) => {
-  res.redirect('/welcome');
-});
-
-app.get('/signup', (req,res) => {
-   res.render("signup");
-});
-
-// After signup, posts to this 
-
-app.post('/submitUser', async (req,res) => {
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
-    var securityAnswer = req.body.answer;
-
-    const schema = Joi.object(
-      {
-        username: Joi.string().alphanum().max(20).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().max(20).required(),
-        securityAnswer: Joi.string().alphanum().max(20).required(),
-      });
-
-    const validationResult = schema.validate({username, email, password, securityAnswer});
-    if (validationResult.error != null) {
-      console.log(validationResult.error);
-        var error = validationResult.error.details[0].message;
-        res.render("signupfail", {theError: error});
-      return;
-    }
-
-      var hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    await userCollection.insertOne({username: username, password: hashedPassword,email: email, Security_Question_Answer: securityAnswer,user_type: 'user', pfp: null, playlists: []});
-    console.log("Inserted user");
-
-      res.redirect('/welcome');
-});
-
-app.get('/welcome', hasSession, (req,res) => {
-  res.render("welcome", {isLoggedIn: req.session.authenticated, userName: req.session.name});
-});
-
-app.get('/changePassword', (req,res) =>{
-      var sessionState = req.session.authenticated;
-      if(sessionState){
-        res.render("resetPassword", {sessionState: sessionState});
-      } else {
-        res.render("securityQuestion");
-      }
-
-});
-
-app.post('/changingPassword', async (req,res) => {
-  
-    var newpassword = req.body.password;
-    var useremail = req.session.email;
-    console.log(useremail);
-    let currentUser = await userCollection.findOne({email: useremail});
-    console.log("hello");
-    console.log(currentUser);
-    console.log(currentUser.email);
-    console.log(currentUser.password);
-    var hashedPassword = await bcrypt.hash(newpassword, saltRounds);
-    await userCollection.updateOne({email: useremail}, {$set: {password: hashedPassword}});
-    console.log("=========NEW PASS=========");
-    console.log(newpassword);
-    console.log(hashedPassword);
-    
-    if (req.session.authenticated){
-      console.log(currentUser.username + "going to welcome");
- 
-      res.render("welcome", {userName: currentUser.username, isLoggedIn: req.session.authenticated});
-
-    }else{
-      res.render("login");
-    }
-
-
-});
-
-app.post('/securityQuestion', async (req,res) => {
+app.post("/securityQuestion", async (req, res) => {
   var useremail = req.body.email;
   req.session.email = useremail;
   var securityans = req.body.answer;
-  let currentUser = await userCollection.findOne({email: useremail});
-  if(!currentUser){
+  let currentUser = await userCollection.findOne({ email: useremail });
+  if (!currentUser) {
     res.render("securityQuestion");
-  }else{
-    if(currentUser.email == useremail && currentUser.Security_Question_Answer == securityans){
+  } else {
+    if (
+      currentUser.email == useremail &&
+      currentUser.Security_Question_Answer == securityans
+    ) {
       res.render("resetPassword");
     }
   }
-  
 });
 
-app.get('/logout', (req,res) => {
-	req.session.destroy();
-    var sessionState = false;
-    var username = "";
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  var sessionState = false;
+  var username = "";
 
-    res.render("welcome", {isLoggedIn: sessionState, userName: username});
+  res.render("welcome", { isLoggedIn: sessionState, userName: username });
 });
-  
+
 app.get("/home", hasSession, (req, res) => {
   res.render("index");
 });
@@ -233,61 +254,93 @@ app.get("/pickTags", hasSession, (req, res) => {
   // These have to be strings
   // TODO: Add more tags. Possibly store them in a database
   // TODO: Find a way to transfer tags between pages
-  var tags = ["test1", "test2"];
-  res.render("pickTags" , {tags: tags, pickedTags: req.session.pickedTags, blacklistedTags: req.session.blacklistedTags});
+  var tags = ["test1", "test2", "tag3", "tag4", "tag5", "tag6"];
+
+  // Get the search query from the URL query parameters
+  const searchQuery = req.query.search || "";
+
+  // Filter the tags based on the search query
+  const filteredTags = tags.filter((tag) =>
+    tag.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  res.render("pickTags", {
+    tags: filteredTags,
+    pickedTags: req.session.pickedTags,
+    blacklistedTags: req.session.blacklistedTags,
+  });
 });
 
+app.post("/resetTags", hasSession, (req, res) => {
+  // Reset the pickedTags and blacklistedTags arrays in the session object
+  req.session.pickedTags = [];
+  req.session.blacklistedTags = [];
 
-  app.post("/updateTags", (req, res) => {
-    const tags = req.body.tags; // Array of selected tags
-    const actions = req.body.actions; // Array of corresponding actions for each tag
+  res.redirect("/pickTags");
+});
 
-      // Retrieve the pickedTags and blacklistedTags arrays from the session object
-    let pickedTags = [];
-    let blacklistedTags = [];
+app.post("/updateTags", hasSession, (req, res) => {
+  const tags = req.body.tags; // Array of selected tags
+  const actions = req.body.actions; // Array of corresponding actions for each tag
 
-    // TODO: Add check for whether the user already inputted a playlist
-    if (typeof tags === 'undefined' || tags.length == 0) {
-      // No tags were selected
-      res.redirect("/confirmTags");
-      return;
-    }
-  
-    for (let i = 0; i < tags.length; i++) {
-      const tag = tags[i];
-      const action = actions[i];
-  
-      // Handle the selected action for each tag
-      if (action === "add") {
-        pickedTags.push(tag); // Add the tag to the pickedTags array
-      } else if (action === "blacklist") {
-        blacklistedTags.push(tag); // Add the tag to the blacklistedTags array
-      } else if (action === "blank") {
-        // Remove the tag from both arrays, if it exists
-        pickedTags = pickedTags.filter((pickedTag) => pickedTag !== tag);
-        blacklistedTags = blacklistedTags.filter((blacklistedTag) => blacklistedTag !== tag);
-      }
-    }
-    
-    // Store the pickedTags and blacklistedTags arrays in the session object
-    req.session.pickedTags = pickedTags;
-    req.session.blacklistedTags = blacklistedTags;
+  console.log("tags: " + tags);
+  console.log("actions: " + actions);
 
-    // Redirect back to the /pickTags page or any other desired page
+  // Retrieve the pickedTags and blacklistedTags arrays from the session object
+  let pickedTags = [];
+  let blacklistedTags = [];
+
+  // TODO: Add check for whether the user already inputted a playlist
+  if (typeof tags === "undefined" || tags.length == 0) {
+    // No tags were selected
     res.redirect("/confirmTags");
-  });
-  
+    return;
+  }
 
+  for (let i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    var action = actions[i];
+
+    // Handle the selected action for each tag
+    if (action === "add") {
+      pickedTags.push(tag); // Add the tag to the pickedTags array
+    } else if (action === "blacklist") {
+      blacklistedTags.push(tag); // Add the tag to the blacklistedTags array
+    } else if (action === "blank") {
+      // Remove the tag from both arrays, if it exists
+      // Note: this might not be needed later
+      pickedTags = pickedTags.filter((pickedTag) => pickedTag !== tag);
+      blacklistedTags = blacklistedTags.filter(
+        (blacklistedTag) => blacklistedTag !== tag
+      );
+    }
+  }
+
+  // Store the pickedTags and blacklistedTags arrays in the session object
+  req.session.pickedTags = pickedTags;
+  req.session.blacklistedTags = blacklistedTags;
+
+  // Redirect back to the /pickTags page or any other desired page
+  res.redirect("/confirmTags");
+});
 
 app.get("/confirmTags", hasSession, (req, res) => {
-  res.render("confirmTags", {pickedTags: req.session.pickedTags, blacklistedTags: req.session.blacklistedTags});
+  console.log("Picked tags length: " + req.session.pickedTags.length);
+  console.log("Blacklisted tags length: " + req.session.blacklistedTags.length);
+  res.render("confirmTags", {
+    pickedTags: req.session.pickedTags,
+    blacklistedTags: req.session.blacklistedTags,
+  });
 });
 
 app.post("/confirmChoices", hasSession, async (req, res) => {
+  // TODO: Put AI stuff here
   res.redirect("/results");
 });
 
 app.get("/results", hasSession, (req, res) => {
+  console.log("PickedTags: " + req.session.pickedTags);
+  console.log("BlacklistedTags: " + req.session.blacklistedTags);
   res.render("results");
 });
 
@@ -308,7 +361,10 @@ app.post("/editUsername", hasSession, async (req, res) => {
     return;
   }
 
-  await userCollection.updateOne({username: req.session.username}, {$set: {username: username}});
+  await userCollection.updateOne(
+    { username: req.session.username },
+    { $set: { username: username } }
+  );
   res.redirect("/profile");
 });
 
@@ -333,9 +389,6 @@ app.post("/editPhoto", async (req, res) => {
     res.status(500).send("Failed to update photo.");
   }
 });
-
-
-
 
 app.use(express.static(__dirname + "/public"));
 
