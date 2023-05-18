f1934eb77fa8ad4b4e4bdecd6b6c5c661dcb0b1d
Kapish <kapishsingla.2004@gmail.com>
2023-05-18 14:22:06 -0700

    Added the AI stuff to AI.js

--- AI.js
+++ AI.js
@@ -1,3 +1,50 @@
 const tf = require('@tensorflow/tfjs');
 const model = tf.sequential();
 file_writer = tf.summary.FileWriter('/logs', sess.graph)
+
+// Import TensorFlow.js
+const tf = require('@tensorflow/tfjs-node');
+
+// Read and preprocess the CSV data
+const csvFilePath = 'path/to/your/csv/file.csv';
+const csvData = '...' // Read the CSV data using your preferred method (e.g., csv-parser, papaparse)
+
+// Prepare the data for training
+const inputData = '...' // Extract input features from csvData
+const targetData = '...' // Extract target labels from csvData
+
+// Split the data into training and testing sets
+const splitRatio = 0.8; // 80% for training, 20% for testing
+const numExamples = inputData.length;
+const numTrainExamples = Math.floor(numExamples * splitRatio);
+
+const trainInput = inputData.slice(0, numTrainExamples);
+const trainTarget = targetData.slice(0, numTrainExamples);
+const testInput = inputData.slice(numTrainExamples);
+const testTarget = targetData.slice(numTrainExamples);
+
+// Define the model architecture
+// const model = tf.sequential();
+model.add(tf.layers.dense({ units: 16, activation: 'relu', inputShape: [inputData[0].length] }));
+model.add(tf.layers.dense({ units: 1 }));
+
+// Compile the model
+model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
+
+// Train the model
+const epochs = 100;
+const batchSize = 32;
+await model.fit(tf.tensor2d(trainInput), tf.tensor2d(trainTarget), {
+  epochs,
+  batchSize,
+  validationData: [tf.tensor2d(testInput), tf.tensor2d(testTarget)],
+});
+
+// Evaluate the model
+const evalOutput = model.evaluate(tf.tensor2d(testInput), tf.tensor2d(testTarget));
+console.log('Loss:', evalOutput.dataSync()[0]);
+
+// Use the trained model to make predictions
+const predictionInput = '...' // Prepare input data for prediction
+const predictions = model.predict(tf.tensor2d(predictionInput));
+predictions.print();
