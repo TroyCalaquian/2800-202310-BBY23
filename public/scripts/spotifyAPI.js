22750e71ce2ab4374ca3a21eaa2193557c2498c5
MaxwellV <113266750+MaxwellVanderhoeven@users.noreply.github.com>
2023-05-18 20:31:45 -0700

    Pushing to enable merge.

--- public/scripts/spotifyAPI.js
+++ public/scripts/spotifyAPI.js
@@ -97,6 +97,18 @@ async function getTracks() {
 //     console.error(err);
 //   });
 
+  let ids = ''
+  spotifyAPI.searchTracks('genre:hip-hop', {limit, offset})
+  .then(function(data) {
+    for(i =0; i <  data.body.tracks.items.length; i++){
+       ids += data.body.tracks.items[i].id + '\n'
+      //  var csvString = Object.values(ids) + '\n'
+       console.log('Search by "Hip-hop"', ids);
+    }
+    // fs.appendFileSync('song_id.csv', ids, 'utf-8');
+  }, function(err) {
+    console.error(err);
+  });
 }
 
 // Search tracks by genre and retrieve the track IDs
